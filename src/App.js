43707ff7e5f684bbbaa2e6f1f6277cf87f180f94
Maheshkhatake <136154669+Maheshkhatake@users.noreply.github.com>
2024-01-26 14:12:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ExpressionForm from './ExpressionForm';
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-center mt-4 mb-5">Expression Form</h2>
+      <ExpressionForm />
+    </div>
+  );
+}
+
+export default App;
